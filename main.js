f5bda1546d037503718d6d90cb4b50baf4fb542a
gub1th <danielfgunawan1@gmail.com>
2023-10-13 14:44:40 -0400

    added boat model, as well as basic controls

--- main.js
+++ main.js
@@ -1,87 +1,138 @@
 import * as THREE from 'three';
 import './style.css';
-import gsap from "gsap";
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
-//create the scene
+// Create the scene
 const scene = new THREE.Scene();
 
-//sizes
+// Sizes
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
 };
 
-//create the sphere
-const geometry = new THREE.SphereGeometry(3,64,64);
-const material = new THREE.MeshStandardMaterial({color: "#00ff83", roughness: 0.3});
-const mesh = new THREE.Mesh(geometry, material);
+let boatScene;
 
-scene.add(mesh);
+// Make GLTF loader
+const gltfLoader = new GLTFLoader();
 
-//light
-const light = new THREE.PointLight(0xffffff, 1, 100);
-light.position.set(0, 10, 10);
-light.intensity = 10;
-scene.add(light);
+// Create the rowboat
+gltfLoader.load('./assets/Rowboat.glb', (boatModel) => {
+  boatModel.scene.scale.set(0.15, 0.15, 0.15);
+  boatModel.scene.rotation.y = Math.PI;
+  boatScene = boatModel.scene;
+  scene.add(boatScene);
+});
 
-//camera
-const camera = new THREE.PerspectiveCamera(45, sizes.width/sizes.height, 0.1, 100);
-camera.position.z = 20;
+// Create the floor
+const floorGeometry = new THREE.PlaneGeometry(50, 50);
+const floorMaterial = new THREE.MeshStandardMaterial({ color: '#aaaaaa', roughness: 0.8 });
+const floor = new THREE.Mesh(floorGeometry, floorMaterial);
+floor.rotation.x = -Math.PI / 2;
+scene.add(floor);
+
+// Light
+const pointLight = new THREE.PointLight(0xffffff, 1, 100);
+pointLight.position.set(0, 10, 10);
+pointLight.intensity = 100;
+scene.add(pointLight);
+
+const lightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(lightHelper, gridHelper);
+
+// Camera
+const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 100);
+camera.position.set(0, 20, 40);
 scene.add(camera);
 
-//Renderer
-const canvas = document.querySelector(".webgl");
-const renderer = new THREE.WebGLRenderer({canvas});
-renderer.setSize(sizes.width,sizes.height)
-renderer.setPixelRatio(2);
-renderer.render(scene, camera);
+// Renderer
+const canvas = document.querySelector('.webgl');
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(window.devicePixelRatio);
 
-//controls
+// Controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 controls.enablePan = false;
 controls.enableZoom = false;
-controls.autoRotate = true;
-controls.autoRotateSpeed = 5;
 
-//reisze
+// Arrow key movement for the boat
+const boatSpeed = 0.1;
+const boatDirection = new THREE.Vector3();
+
+window.addEventListener('keydown', (event) => {
+  switch (event.key) {
+    case 'ArrowUp':
+      boatDirection.z = -boatSpeed;
+      boatScene.rotation.y = Math.PI;
+      break;
+    case 'ArrowDown':
+      boatDirection.z = boatSpeed;
+      boatScene.rotation.y = 0;
+      break;
+    case 'ArrowLeft':
+      boatDirection.x = -boatSpeed;
+      boatScene.rotation.y = -Math.PI/2;
+      break;
+    case 'ArrowRight':
+      boatDirection.x = boatSpeed;
+      boatScene.rotation.y = Math.PI/2;
+      break;
+  }
+});
+
+window.addEventListener('keyup', (event) => {
+  switch (event.key) {
+    case 'ArrowUp':
+    case 'ArrowDown':
+      boatDirection.z = 0;
+      break;
+    case 'ArrowLeft':
+    case 'ArrowRight':
+      boatDirection.x = 0;
+      break;
+  }
+});
+
+// Resize
 window.addEventListener('resize', () => {
   sizes.width = window.innerWidth;
   sizes.height = window.innerHeight;
   camera.updateProjectionMatrix();
-  camera.aspect = sizes.width/sizes.height;
+  camera.aspect = sizes.width / sizes.height;
   renderer.setSize(sizes.width, sizes.height);
-})
+});
 
-const loop = () => {
+const animate = () => {
+  window.requestAnimationFrame(animate);
+
+  // Update boat position and rotation
+  if (boatScene) {
+    boatScene.position.add(boatDirection);
+
+    // Calculate the new camera position relative to the boat
+    const offset = new THREE.Vector3(0, 20, 40); // Set your desired offset
+    const boatPosition = boatScene.position.clone();
+
+    // Calculate the camera position
+    const cameraPosition = boatPosition.clone().add(offset);
+
+    camera.position.copy(cameraPosition);
+    camera.lookAt(boatPosition); // Make the camera look at the boat
+
+    // Update the camera matrix for orbit controls
+    controls.target.copy(boatPosition);
+
+    console.log(cameraPosition, boatPosition);
+  }
+
   controls.update();
   renderer.render(scene, camera);
-  window.requestAnimationFrame(loop);
-}
+};
 
-loop();
+animate();
 
-//timeline magic
-const tl = gsap.timeline({defaults: {duration: 1}});
-tl.fromTo(mesh.scale, {z:0,x:0,y:0}, {z:1,x:1,y:1});
-tl.fromTo("nav", {y: "-75%"}, {y: "0%"});
-tl.fromTo(".title", {opacity: 0}, {opacity: 1});
-
-//Mouse Animation color
-let mouseDown = false;
-let rgb = [];
-window.addEventListener("mousedown", () => (mouseDown = true));
-window.addEventListener("mouseup", () => (mouseDown = false));
-
-window.addEventListener("mousemove", (e) => {
-  if (mouseDown) {
-    rgb = [
-      Math.round((e.pageX / sizes.width) * 255),
-      Math.round((e.pageY / sizes.height) * 255),
-      150,
-    ]
-    let newColor = new THREE.Color(`rgb(${rgb.join(",")})`);
-    gsap.to(mesh.material.color, {r:newColor.r,g:newColor.g,b:newColor.b});
-  }
-})
+animate();
